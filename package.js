5abbaf9a835b85913794d63bf59f646ae6f257b2
S <msj242@gmail.com>
2016-12-07 23:01:21 -0500

    Update package.js
    Updated Version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'tomi:upload-server',
   summary: 'Upload server for Meteor. Allows to save and serve files from arbitrary directory',
-  version: '1.3.4_2',
+  version: '1.3.4_3',
   git: 'https://github.com/tomitrescak/meteor-tomi-upload-server.git'
 });
 
